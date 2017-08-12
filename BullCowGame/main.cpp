633ecc9ea6fb28a7d5c84c0dfd9406533ed628b5
adam.horvath.voros <adam.horvath.voros@gmail.com>
2017-08-12 12:12:23 +1200

    First commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	constexpr int WORD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, the word game of your lifetime!" << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
+	return 0;
+}
